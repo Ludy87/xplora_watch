ea4626b938544f0069bdf7ca9fbc08752d1106af
Ludy <1@astra-g.org>
2022-01-04 03:12:59 +0100

    Create update_readme.py

---
+++ .github/actions/update_readme.py
@@ -0,0 +1,16 @@
+import os
+from pyxplora_api import pyxplora_api as PXA
+
+country_code = "+49"
+phonenumber = os.environ['PHONENUMBER']
+password = os.environ['PASSWORD']
+userlang = "de-DE"
+timezone = "Europe/Berlin"
+
+controller = PXA.PyXploraApi(country_code, phonenumber, password, userlang, timezone)
+
+with open("countries.md", "w") as text_file:
+    text_file.write("| country name | country code |\n")
+    text_file.write("|--------------|--------------|\n")
+    for country in controller.handler.countries()["countries"]:
+        text_file.write(f"| {country['name']} | {country['code']} |\n")
