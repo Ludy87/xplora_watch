7028f37525044dcb864926787b09ad7ae304397c
Ludy87 <Ludy87@users.noreply.github.com>
2022-04-30 21:31:23 +0200

    🔨 country
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>

--- .github/actions/update_readme.py
+++ .github/actions/update_readme.py
@@ -1,18 +1,9 @@
-import os
-from pyxplora_api import pyxplora_api as PXA
-
-country_code = "+49"
-phonenumber = os.environ["PHONENUMBER"]
-password = os.environ["PASSWORD"]
-userlang = "de-DE"
-timezone = "Europe/Berlin"
-
-controller = PXA.PyXploraApi(country_code, phonenumber, password, userlang, timezone)
+import json
 
 with open("countries.md", "w") as text_file:
     text_file.write("| country name | country code |\n")
     text_file.write("|--------------|--------------|\n")
-    text_file.write("| Denmark | 45 |\n")
-    text_file.write("| Netherlands | 31 |\n")
-    for country in controller.handler.countries()["countries"]:
-        text_file.write(f"| {country['name']} | {country['code']} |\n")
+    with open(".github\\actions\\country.json", "r") as json_file:
+        countries = json.loads(json_file.read())
+        for country in countries:
+            text_file.write(f"| {country['name']} | {country['code']} |\n")
