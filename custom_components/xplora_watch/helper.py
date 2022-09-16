7664099a5c9c1707afafd0bffd12c9b32b253f7d
Ludy87 <Ludy87@users.noreply.github.com>
2022-09-16 21:42:26 +0200

    🌱 location distance
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>

--- custom_components/xplora_watch/helper.py
+++ custom_components/xplora_watch/helper.py
@@ -1,7 +1,14 @@
-"""HelperClasses Xplora® Watch."""
+"""HelperClasses Xplora® Watch Version 2."""
 from __future__ import annotations
 
 from datetime import datetime, timedelta
+from geopy import distance
+
+from .const import ATTR_TRACKER_LATITUDE, ATTR_TRACKER_LONGITUDE, HOME
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class XploraUpdateTime:
@@ -20,3 +27,25 @@ class XploraDevice(XploraUpdateTime):
     def __init__(self, scan_interval: timedelta, start_time: float) -> None:
         """Set up the Xplora® device."""
         super().__init__(scan_interval, start_time)
+
+
+def get_location_distance_meter(hass, lat_lng: tuple[float, float]) -> int:
+    home_zone = hass.states.get(HOME).attributes
+    return int(
+        distance.distance(
+            (home_zone[ATTR_TRACKER_LATITUDE], home_zone[ATTR_TRACKER_LONGITUDE]),
+            lat_lng,
+        ).m
+    )
+
+
+def get_location_distance(home_lat_lng: tuple[float, float], lat_lng: tuple[float, float], radius: int) -> int:
+    if radius >= int(
+        distance.distance(
+            home_lat_lng,
+            lat_lng,
+        ).m
+    ):
+        return True
+    else:
+        return False
