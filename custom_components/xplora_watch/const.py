5c33a57b93739e2dbaa02464323bd08b6d0ae221
Ludy87 <Ludy87@users.noreply.github.com>
2022-09-17 14:53:27 +0200

    Update const.py

--- custom_components/xplora_watch/const.py
+++ custom_components/xplora_watch/const.py
@@ -1,64 +1,65 @@
 """Const for Xplora® Watch Version 2."""
-DOMAIN = "xplora_watch"
-MANUFACTURER = "Xplora®"
-DEVICE_NAME = "Xplora® Watch"
-ATTRIBUTION = "Data provided by Xplora®"
+from __future__ import annotations
 
-ATTR_TRACKER_ADDR = "address"
-ATTR_TRACKER_DISTOHOME = "Home Distance (m)"
-ATTR_TRACKER_IMEI = "imei"
-ATTR_TRACKER_LAST_TRACK = "last tracking"
-ATTR_TRACKER_LAT = "lat"
-ATTR_TRACKER_LATITUDE = "latitude"
-ATTR_TRACKER_LICENCE = "licence"
-ATTR_TRACKER_LNG = "lng"
-ATTR_TRACKER_LONGITUDE = "longitude"
-ATTR_TRACKER_POI = "poi"
-ATTR_TRACKER_RAD = "rad"
+from typing import Final
 
-ATTR_WATCH = "watch"
+DOMAIN: Final = "xplora_watch_2"
+MANUFACTURER: Final = "Xplora®"
+DEVICE_NAME: Final = "Xplora® Watch"
+ATTRIBUTION: Final = "Data provided by Xplora®"
 
-CONF_COUNTRY_CODE = "country_code"
-CONF_HOME_SAFEZONE = "home_is_safezone"
-CONF_HOME_LATITUDE = "home_latitude"
-CONF_HOME_LONGITUDE = "home_longitude"
-CONF_HOME_RADIUS = "home_radius"
-CONF_MAPS = "maps"
-CONF_OPENCAGE_APIKEY = "opencage_apikey"
-CONF_PASSWORD = "password"
-CONF_PHONENUMBER = "phonenumber"
-CONF_TIMEZONE = "timezone"
-CONF_TYPES = "types"
-CONF_USERLANG = "userlang"
-CONF_WATCHES = "watches"
+ATTR_TRACKER_ADDR: Final = "address"
+ATTR_TRACKER_DISTOHOME: Final = "Home Distance (m)"
+ATTR_TRACKER_IMEI: Final = "imei"
+ATTR_TRACKER_LAST_TRACK: Final = "last tracking"
+ATTR_TRACKER_LAT: Final = "lat"
+ATTR_TRACKER_LICENCE: Final = "licence"
+ATTR_TRACKER_LNG: Final = "lng"
+ATTR_TRACKER_POI: Final = "poi"
+ATTR_TRACKER_RAD: Final = "rad"
 
-DAYS = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"]
-HOME = "zone.home"
+ATTR_WATCH: Final = "watch"
 
-SENSOR_BATTERY = "battery"
-SENSOR_STEP_DAY = "step_day"
-SENSOR_XCOIN = "xcoin"
+CONF_COUNTRY_CODE: Final = "country_code"
+CONF_HOME_SAFEZONE: Final = "home_is_safezone"
+CONF_HOME_LATITUDE: Final = "home_latitude"
+CONF_HOME_LONGITUDE: Final = "home_longitude"
+CONF_HOME_RADIUS: Final = "home_radius"
+CONF_MAPS: Final = "maps"
+CONF_OPENCAGE_APIKEY: Final = "opencage_apikey"
+CONF_PHONENUMBER: Final = "phonenumber"
+CONF_TIMEZONE: Final = "timezone"
+CONF_TYPES: Final = "types"
+CONF_USERLANG: Final = "userlang"
+CONF_WATCHES: Final = "watches"
 
-BINARY_SENSOR_CHARGING = "charging"
-BINARY_SENSOR_SAFEZONE = "safezone"
-BINARY_SENSOR_STATE = "state"
+DAYS: Final = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"]
+HOME: Final = "zone.home"
 
-SWITCH_ALARMS = "alarms"
-SWITCH_SILENTS = "silents"
+SENSOR_BATTERY: Final = "battery"
+SENSOR_STEP_DAY: Final = "step_day"
+SENSOR_XCOIN: Final = "xcoin"
 
-DEVICE_TRACKER_SAFZONES = "watch_safezone"
-DEVICE_TRACKER_WATCH = "dt_watch"
+BINARY_SENSOR_CHARGING: Final = "charging"
+BINARY_SENSOR_SAFEZONE: Final = "safezone"
+BINARY_SENSOR_STATE: Final = "state"
 
-DEFAULT_SCAN_INTERVAL = 3 * 60
-TRACKER_UPDATE = 60
+SWITCH_ALARMS: Final = "alarms"
+SWITCH_SILENTS: Final = "silents"
 
-TRACKER_UPDATE_STR = f"{DOMAIN}_tracker_update"
+DEVICE_TRACKER_SAFZONES: Final = "watch_safezone"
+DEVICE_TRACKER_WATCH: Final = "dt_watch"
 
-DATA_HASS_CONFIG = "hass_config"
+DEFAULT_SCAN_INTERVAL: Final = 3 * 60
+TRACKER_UPDATE: Final = 60
 
-HOME_SAFEZONE = {"no": "no", "yes": "yes"}
-MAPS = ["openstreetmap.org (free)", "opencagedata.com (with Licence)"]
-SENSORS = {
+TRACKER_UPDATE_STR: Final = f"{DOMAIN}_tracker_update"
+
+DATA_HASS_CONFIG: Final = "hass_config"
+
+HOME_SAFEZONE: Final[dict[str, str]] = {"off": "off", "on": "on"}
+MAPS: Final[list[str]] = ["openstreetmap.org (free)", "opencagedata.com (with Licence)"]
+SENSORS: Final[dict[str, str]] = {
     BINARY_SENSOR_CHARGING: "show charging",
     BINARY_SENSOR_SAFEZONE: "is in Safezone",
     BINARY_SENSOR_STATE: "online State",
