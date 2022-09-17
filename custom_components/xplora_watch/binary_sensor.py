35188752025b324d0e252604a7c364d8cf320405
Ludy87 <Ludy87@users.noreply.github.com>
2022-09-17 14:41:24 +0200

    🌱 final var
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>

--- custom_components/xplora_watch/binary_sensor.py
+++ custom_components/xplora_watch/binary_sensor.py
@@ -9,6 +9,7 @@ from homeassistant.components.binary_sensor import (
     BinarySensorEntityDescription,
 )
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import ATTR_ID, ATTR_LATITUDE, ATTR_LONGITUDE, CONF_NAME, STATE_OFF, STATE_ON
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -16,9 +17,7 @@ import logging
 
 from .const import (
     ATTR_TRACKER_LAT,
-    ATTR_TRACKER_LATITUDE,
     ATTR_TRACKER_LNG,
-    ATTR_TRACKER_LONGITUDE,
     ATTR_WATCH,
     BINARY_SENSOR_CHARGING,
     BINARY_SENSOR_SAFEZONE,
@@ -66,7 +65,7 @@ async def async_setup_entry(
         for watch in coordinator.controller.watchs:
             if config_entry.options:
                 ward: Dict[str, Any] = watch.get("ward")
-                uid = ward.get("id")
+                uid = ward.get(ATTR_ID)
                 if uid in config_entry.options.get(CONF_WATCHES):
                     if description.key in config_entry.options.get(CONF_TYPES):
                         sw_version = await coordinator.controller.getWatches(uid)
@@ -91,8 +90,8 @@ class XploraBinarySensor(XploraBaseEntity, BinarySensorEntity):
     ) -> None:
         super().__init__(coordinator, ward, sw_version, uid)
         self.entity_description = description
-        self._attr_name = f'{self._ward.get("name")} {ATTR_WATCH} {description.key} {uid}'.title()
-        self._attr_unique_id = f'{self._ward.get("name")}-{ATTR_WATCH}-{description.key}-{uid}'
+        self._attr_name = f'{self._ward.get(CONF_NAME)} {ATTR_WATCH} {description.key} {uid}'.title()
+        self._attr_unique_id = f'{self._ward.get(CONF_NAME)}-{ATTR_WATCH}-{description.key}-{uid}'
         self._config_entry = config_entry
         _LOGGER.debug(
             "Updating binary_sensor: %s | %s | Watch_ID %s",
@@ -110,11 +109,11 @@ class XploraBinarySensor(XploraBaseEntity, BinarySensorEntity):
             return self._coordinator.watch_entry[self.watch_uid]["isOnline"]
         if self.entity_description.key == BINARY_SENSOR_SAFEZONE:
             _options = self._config_entry.options
-            if _options.get(CONF_HOME_SAFEZONE, "no") == "yes":
+            if _options.get(CONF_HOME_SAFEZONE, STATE_OFF) == STATE_ON:
                 latitude = self._coordinator.watch_entry[self.watch_uid][ATTR_TRACKER_LAT]
                 longitude = self._coordinator.watch_entry[self.watch_uid][ATTR_TRACKER_LNG]
-                home_latitude = self.hass.states.get(HOME).attributes[ATTR_TRACKER_LATITUDE]
-                home_longitude = self.hass.states.get(HOME).attributes[ATTR_TRACKER_LONGITUDE]
+                home_latitude = self.hass.states.get(HOME).attributes[ATTR_LATITUDE]
+                home_longitude = self.hass.states.get(HOME).attributes[ATTR_LONGITUDE]
                 home_raduis = self.hass.states.get(HOME).attributes["radius"]
                 if get_location_distance(
                     (
