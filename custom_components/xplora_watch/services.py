52548b12adb2d4d656f5fb7b8d0b0f9524321af4
Ludy <Ludy87@users.noreply.github.com>
2022-11-13 13:50:27 +0100

    ✨ Shutdown Watch per Service (v2.5.0) (#111)
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>
    
    Signed-off-by: Ludy87 <Ludy87@users.noreply.github.com>

--- custom_components/xplora_watch/services.py
+++ custom_components/xplora_watch/services.py
@@ -5,6 +5,7 @@ import logging
 from typing import Any
 
 import voluptuous as vol
+from pyxplora_api.exception_classes import NoAdminError
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.core import HomeAssistant, ServiceCall, callback
@@ -14,6 +15,7 @@ from .const import (
     ATTR_SERVICE_READ_MSG,
     ATTR_SERVICE_SEE,
     ATTR_SERVICE_SEND_MSG,
+    ATTR_SERVICE_SHUTDOWN,
     ATTR_SERVICE_TARGET,
     CONF_MESSAGE,
     DOMAIN,
@@ -21,10 +23,13 @@ from .const import (
 )
 from .coordinator import XploraDataUpdateCoordinator
 
+BASE_SHUTDOWN_SERVICE_SCHEMA = vol.Schema(
+    {vol.Required(ATTR_SERVICE_TARGET): vol.All(cv.ensure_list, [cv.string])}, extra=vol.ALLOW_EXTRA
+)
 BASE_READ_MESSAGE_SERVICE_SCHEMA = vol.Schema(
     {vol.Required(ATTR_SERVICE_TARGET): vol.All(cv.ensure_list, [cv.string])}, extra=vol.ALLOW_EXTRA
 )
-BASE_NOTIFY_SERVICE_SCHEMA = vol.Schema(
+BASE_SEND_MESSAGE_SERVICE_SCHEMA = vol.Schema(
     {vol.Required(ATTR_SERVICE_TARGET): vol.All(cv.ensure_list, [cv.string]), vol.Required(ATTR_SERVICE_MSG): cv.string},
     extra=vol.ALLOW_EXTRA,
 )
@@ -39,6 +44,7 @@ _LOGGER = logging.getLogger(__name__)
 def async_setup_services(hass: HomeAssistant, coordinator: XploraDataUpdateCoordinator) -> None:
     """Set up services for Xplora® Watch integration."""
 
+    shutdown_service = XploraShutdownService(hass, coordinator)
     sensor_update_service = XploraMessageSensorUpdateService(hass, coordinator)
     notify_service = XploraMessageService(hass, coordinator)
     see_service = XploraSeeService(hass, coordinator)
@@ -47,7 +53,7 @@ def async_setup_services(hass: HomeAssistant, coordinator: XploraDataUpdateCoord
         kwargs = dict(service.data)
         await see_service.async_see(kwargs[ATTR_SERVICE_TARGET] if ATTR_SERVICE_TARGET in kwargs else ["all"])
 
-    async def async_send_xplora_message(service: ServiceCall) -> None:
+    async def async_send_message(service: ServiceCall) -> None:
         kwargs = dict(service.data)
         await notify_service.async_send_message(kwargs[ATTR_SERVICE_MSG], kwargs[ATTR_SERVICE_TARGET])
 
@@ -55,14 +61,20 @@ def async_setup_services(hass: HomeAssistant, coordinator: XploraDataUpdateCoord
         kwargs = dict(service.data)
         await sensor_update_service.async_read_message(kwargs[ATTR_SERVICE_TARGET])
 
+    async def async_shutdown(service: ServiceCall) -> None:
+        kwargs = dict(service.data)
+        await shutdown_service.async_shutdown(kwargs[ATTR_SERVICE_TARGET])
+
+    hass.services.async_register(DOMAIN, ATTR_SERVICE_SHUTDOWN, async_shutdown, schema=BASE_SHUTDOWN_SERVICE_SCHEMA)
     hass.services.async_register(DOMAIN, ATTR_SERVICE_READ_MSG, async_read_message, schema=BASE_READ_MESSAGE_SERVICE_SCHEMA)
-    hass.services.async_register(DOMAIN, ATTR_SERVICE_SEND_MSG, async_send_xplora_message, schema=BASE_NOTIFY_SERVICE_SCHEMA)
+    hass.services.async_register(DOMAIN, ATTR_SERVICE_SEND_MSG, async_send_message, schema=BASE_SEND_MESSAGE_SERVICE_SCHEMA)
     hass.services.async_register(DOMAIN, ATTR_SERVICE_SEE, async_see, schema=BASE_SEE_SERVICE_SCHEMA)
 
 
 @callback
 def async_unload_services(hass: HomeAssistant) -> None:
     """Unload Xplora® Watch send_message services."""
+    hass.services.async_remove(DOMAIN, ATTR_SERVICE_SHUTDOWN)
     hass.services.async_remove(DOMAIN, ATTR_SERVICE_READ_MSG)
     hass.services.async_remove(DOMAIN, ATTR_SERVICE_SEND_MSG)
     hass.services.async_remove(DOMAIN, ATTR_SERVICE_SEE)
@@ -79,7 +91,8 @@ class XploraSeeService(XploraService):
     def __init__(self, hass: HomeAssistant, coordinator: XploraDataUpdateCoordinator) -> None:
         super().__init__(hass, coordinator)
 
-    async def async_see(self, targets=None, **kwargs):
+    async def async_see(self, targets: list[str] | None = None, **kwargs):
+        """Update all information from Watch"""
         if isinstance(targets, list):
             if "all" in targets:
                 targets = self._controller.getWatchUserIDs()
@@ -95,7 +108,7 @@ class XploraMessageService(XploraService):
     def __init__(self, hass: HomeAssistant, coordinator: XploraDataUpdateCoordinator) -> None:
         super().__init__(hass, coordinator)
 
-    async def async_send_message(self, message="", targets=None, **kwargs):
+    async def async_send_message(self, message="", targets: list[str] | None = None, **kwargs):
         """Send a message to one Watch."""
         if isinstance(targets, list):
             msg = message.strip()
@@ -117,6 +130,7 @@ class XploraMessageSensorUpdateService(XploraService):
         super().__init__(hass, coordinator)
 
     async def async_read_message(self, targets: list[str] | None = None, **kwargs):
+        """Read the messages from account"""
         if isinstance(targets, list):
             old_state: dict[str, Any] = self._coordinator.data
             options = self._coordinator.config_entry.options
@@ -130,5 +144,24 @@ class XploraMessageSensorUpdateService(XploraService):
                     w.update({SENSOR_MESSAGE: (await self._controller.getWatchChatsRaw(watch, limit=limit)).get("chatsNew")})
                 old_state.update({watch: w})
             self._coordinator.async_set_updated_data(old_state)
+        else:
+            _LOGGER.warning(f"No watch id or type '{type(targets)}' not allow!")
+
+
+class XploraShutdownService(XploraService):
+    def __init__(self, hass: HomeAssistant, coordinator: XploraDataUpdateCoordinator) -> None:
+        super().__init__(hass, coordinator)
+
+    async def async_shutdown(self, targets: list[str] | None = None, **kwargs):
+        """turn off watch"""
+        if isinstance(targets, list):
+            if "all" in targets:
+                targets = self._controller.getWatchUserIDs()
+            for watch in targets:
+                await self._controller.init(True)
+                try:
+                    _LOGGER.debug(await self._controller.shutdown(watch))
+                except NoAdminError as err:
+                    _LOGGER.warning(f" Shutdown fail! You have '{err}' Account!")
         else:
             _LOGGER.warning(f"No watch id or type '{type(targets)}' not allow!")
