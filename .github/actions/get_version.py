b909b5fe06250804683c6c1180b9208c73ff1e2c
Ludy87 <Ludy87@users.noreply.github.com>
2023-09-01 12:18:55 +0200

    Update get_version.py

--- .github/actions/get_version.py
+++ .github/actions/get_version.py
@@ -1,11 +1,13 @@
+"""Get version from manifest.json file."""
 import json
 import sys
 
 
 def main():
+    """main."""
     with open("./custom_components/xplora_watch/manifest.json", encoding="utf8") as json_file:
         data = json.load(json_file)
-        print(data["version"])
+        print(data["version"])  # noqa: T201
     return 0
 
 
